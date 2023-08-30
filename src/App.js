32ad3a74beeb35ae72afcff156cc37799d20745a
edsontecjr <125706537+edsontecjr@users.noreply.github.com>
2023-08-30 00:08:30 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Calculator = () => {
+  const [result, setResult] = useState(0);
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+
+  const handleNum1Change = (e) => {
+    setNum1(e.target.value);
+  };
+
+  const handleNum2Change = (e) => {
+    setNum2(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setResult(Number(num1) + Number(num2));
+  };
+
+  const handleSubtract = () => {
+    setResult(Number(num1) - Number(num2));
+  };
+
+  const handleMultiply = () => {
+    setResult(Number(num1) * Number(num2));
+  };
+
+  const handleDivide = () => {
+    setResult(Number(num1) / Number(num2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <input type="number" value={num1} onChange={handleNum1Change} />
+      <input type="number" value={num2} onChange={handleNum2Change} />
+      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleSubtract}>Subtract</button>
+      <button onClick={handleMultiply}>Multiply</button>
+      <button onClick={handleDivide}>Divide</button>
+      <p>Result: {result}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
